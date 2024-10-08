91a73af2b110cf39fef549402ff940b870a4c8ed
Miłosz Gołębiewski <develimgo@gmail.com>
2024-10-08 21:19:24 +0200

    adding functions boilerplate

--- FP_block.py
+++ FP_block.py
@@ -1,14 +1,11 @@
 def pre_FP(RLO_obj, BLOCK_obj):
-	RLO_obj["STACK"].append(2)
-	print("-->FP push rlo")
-	return RLO_obj
-	
-def post_FP(RLO_obj, BLOCK_obj):
-	print("-->FP pop rlo")
-	RLO_obj["RLO"] = RLO_obj["STACK"].pop()
+	print("-->FP pre")
 	return RLO_obj
 	
 def FP(RLO_obj, BLOCK_obj):
-	RLO_obj["RLO"] = RLO_obj["RLO"] + 1
-	print("-->FP execute " + str(RLO_obj["RLO"]))
+	print("-->FP exec")
+	return RLO_obj
+	
+def post_FP(RLO_obj, BLOCK_obj):
+	print("-->FP post")
 	return RLO_obj
